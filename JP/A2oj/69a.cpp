42157eab7fade26f8450611734a3979bdd48ab96
JayPOS <jayp@LAPTOP-JPE271H5.localdomain>
2020-02-10 23:00:34 -0500

    Young Phisicist

--- JP/A2oj/69a.cpp
+++ JP/A2oj/69a.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 int main()
 {
-	int qtd, aux, result = 0;
+	int qtd, aux, result[3];
+	result[0] = result[1] = result[2] = 0;
 	cin >> qtd;
 
 	for(int i = 0; i < qtd*3; i++)
 	{
 		cin >> aux;
-		result += aux;
+		result[i%3] += aux;
 	}
-	if (result == 0)
+	if (result[0] == result[1] && result[1] == result[2] && result[2] == 0)
 		printf("YES\n");
 	else
 		printf("NO\n");
