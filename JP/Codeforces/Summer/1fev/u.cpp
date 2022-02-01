33b142fa5dd40450f4e29c86703201cf82f45676
joaopedro.ribeiro <joaopedro.ribeiro@dock.tech>
2022-02-01 19:22:09 -0300

    passing changes in 1fev to git

--- JP/Codeforces/Summer/1fev/u.cpp
+++ JP/Codeforces/Summer/1fev/u.cpp
@@ -27,24 +27,54 @@ void solve()
     for (int i = 0; i < n; i++) cin >> s[i];
     for (int i = 0; i < n; i++) cin >> q[i];
 
+    for (int i = 0; i < n; i++) cout << s[i] << " " << q[i] << ENDL;
+    cout << ENDL;
     // for (int i = 0; i < n; i++) cout << s1[i] << " " << s2[i] << ENDL; 
     set<string> t;
+    vi resp;
+    int ans = 0;
     for(int i = 0; i < n; i++)
     {
+        cout << s[i] << " " << q[i] << ENDL;
         if (t.empty())
         {
             if (s[i] == q[i])
-                cout << "1 ";
+                resp.push_back(1);
             else
+            {
                 t.insert(s[i]);
+                cout << s[i] << " s[i] nao tem no set, adiciona\n";
+            }
         }
         else
         {
-            if (t.find(s[i]) != t.end())
+            ans++;
+            set<string>::iterator its = t.find(s[i]);
+            set<string>::iterator itq = t.find(q[i]);
+            if (itq == t.end())
             {
+                t.insert(q[i]);
+            }
+            else
+            {
+                cout << q[i] << " q[i] tem no set, remove\n";
+                t.erase(its);
+            }
+            if (t.find(s[i]) == t.end())
+            {
+                cout << s[i] << " s[i] nao tem no set, adiciona\n";
+                t.insert(s[i]);
+            }
+            if(t.empty())
+            {
+                cout << "fechou esse set, vai pro proximo\n";
+                resp.push_back(ans);
+                ans = 0;
             }
         }
     }
+
+    cout << ENDL;
 }
  
 int main() {
